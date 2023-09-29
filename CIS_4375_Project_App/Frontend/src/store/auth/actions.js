950d7f1d85c95d4b623bf8660058778879175dd0
Yashua Saguin <yashuasaguin@gmail.com>
2023-09-29 08:03:37 -0500

    Added notification for users for success and failed login

--- CIS_4375_Project_App/Frontend/src/store/auth/actions.js
+++ CIS_4375_Project_App/Frontend/src/store/auth/actions.js
@@ -6,19 +6,20 @@ export const login = async ({ commit }, { email, password }) => {
         { END_USER_EMAIL: email, 
         END_USER_PASSWORD: password });
 
-        if (response) {
-            // Successful login
+        if(response && response.data.ACTIVE_STATUS_ID === 1) {
             const userData = {
                 isAuthenticated: true,
                 userFirstName: response.data.END_USER_FIRST_NAME,
                 userEmail: response.data.END_USER_EMAIL,
                 userRole: response.data.USER_ROLE_NAME,
-            };
-
+            }
             commit('SET_STATE', userData)
-        }
+            return userData;
+        } else {
+            console.error('Credentials not valid.')
+        };
 
-    } catch (error) {
+    } catch(error) {
         console.error('API request failed:', error);
     }
 }
