0616b44bf4228c49f021b741b361cf25720085a9
TjaartNaude <77753514+TjaartNaude@users.noreply.github.com>
2023-10-08 20:15:33 -0500

    Settings_Delete_Feature

--- CIS_4375_Project_App/Frontend/src/pages/SettingsPage.vue
+++ CIS_4375_Project_App/Frontend/src/pages/SettingsPage.vue
@@ -2,8 +2,12 @@
     <div class="q-pa-md">
         <div class="q-gutter-y-md" style="margin-left: 15px; margin-right: 15px;">
             <q-card>
-                <q-tabs v-model="tab" dense class="text-white" indicator-color="white" :align="'justify'" narrow-indicator
-                    style="background-color: #ad0000;">
+                <q-tabs 
+                    v-model="tab"  
+                    class="text-white bg-secondary" 
+                    indicator-color="white" 
+                    :align="'justify'" 
+                    narrow-indicator>
                     <q-tab name="categories" label="Categories" />
                     <q-tab name="priorities" label="Priorities" />
                 </q-tabs>
@@ -15,42 +19,70 @@
                     <q-tab-panel name="categories">
                         <div class="q-pa-md">
                             <!-- Categries Table -->
-                            <q-table title="Category Types" color="secondary" :align="left" :loading="loading"
-                                :rows="categoryData" :columns="catColumns" style="width: 80%;">
+                            <q-table 
+                                :class="my-sticky-virtscroll-table"
+                                title="Category Types" 
+                                :key="tableKey"
+                                color="secondary"
+                                class="cat-sticky-virtscroll-table"
+                                virtual-scroll
+                                :align="left" 
+                                :loading="loading"
+                                :rows="categoryData" 
+                                :columns="catColumns" 
+                                :virtual-scroll-sticky-size-start="20"
+                                :visible-columns="visibleCategoryColumns" 
+                                style="width: 80%;">
                                 <template #body-cell-status="props">
                                     <q-td :props="props">
-                                        <q-chip :color="props.row.ACTIVE_STATUS_ID === 1 ? 'green' : 'red'"
-                                            text-color="white" dense class="text-weight-bolder" square>{{
-                                                props.row.ACTIVE_STATUS_DESC }}</q-chip>
+                                        <q-chip 
+                                            :color="props.row.ACTIVE_STATUS_ID === 1 ? 'green' : 'red'"
+                                            text-color="white" dense class="text-weight-bolder" square>
+                                            {{props.row.ACTIVE_STATUS_DESC }}
+                                        </q-chip>
                                     </q-td>
                                 </template>
                                 <template v-slot:body-cell-actions="props">
                                     <q-td :props="props">
-                                        <q-btn dense round flat @click="editRow('Category', props)" icon="edit"
-                                            style="color: #ad0000;"></q-btn>
+                                        <q-btn 
+                                            dense 
+                                            round 
+                                            flat 
+                                            color="secondary"
+                                            @click="editRow('Category', props)" 
+                                            icon="edit">
+                                        </q-btn>
                                     </q-td>
                                 </template>
-                                
+                                <template v-slot:body-cell-delactions="props" >
+                                    <q-td :props="props">
+                                        <q-btn 
+                                            dense 
+                                            round 
+                                            flat 
+                                            @click="deleteRow('Category', props)" 
+                                            icon="delete"
+                                            color="secondary"></q-btn>
+                                    </q-td>
+                                </template>
                             </q-table>
-
-                            
-
                         </div>
-                        <q-btn v-if="userRole==='System Administrator' || userRole==='IT Teacher'"
-                        outline icon="person_add" text-color="primary" label="Add New Category"
-                        style="min-width: 140px; background-color: #03521c;" 
-                        class="text-left"
-                        />
 
                     </q-tab-panel>
-    
                     <!-- 
                     Ticket Priorities panel
                     -->
                     <q-tab-panel name="priorities">
                         <!-- Priorities Table -->
-                        <q-table title="Prioirities" color="secondary" :align="left" :loading="loading" :rows="priorityData"
-                            :columns="prioritiesColumns" style="width: 80%;">
+                        <q-table 
+                            title="Prioirities" 
+                            color="secondary" 
+                            :key="catTableKey"
+                            :align="left" 
+                            :loading="loading" 
+                            :rows="priorityData"
+                            :columns="prioritiesColumns" 
+                            style="width: 80%;">
                             <template #body-cell-status="props">
                                 <q-td :props="props">
                                     <q-chip :color="props.row.ACTIVE_STATUS_ID === 1 ? 'green' : 'red'" text-color="white"
@@ -58,11 +90,27 @@
                                 </q-td>
                             </template>
                             <template v-slot:body-cell-actions="props">
-                                <q-td :props="props">
-                                    <q-btn dense round flat @click="editRow('Priority', props)" icon="edit"
-                                        style="color: #ad0000;"></q-btn>
+                                <q-td :props="props" >
+                                    <q-btn 
+                                        dense 
+                                        round 
+                                        flat 
+                                        @click="editRow('Priority', props)" 
+                                        icon="edit"
+                                        color="secondary"></q-btn>
                                 </q-td>
                             </template>
+                            <template v-slot:body-cell-delactions="props">
+                                    <q-td :props="props" >
+                                        <q-btn 
+                                            dense 
+                                            round 
+                                            flat
+                                            @click="deleteRow('Priority', props)" 
+                                            icon="delete"
+                                            color="secondary"></q-btn>
+                                    </q-td>
+                                </template>
                         </q-table>
                     </q-tab-panel>
                 </q-tab-panels>
@@ -79,6 +127,17 @@
                     @save-dialog="handleSaveDialog"
                 />
             </template>
+
+            <!-- Delete Settings Dialog -->
+            <template v-if="deleteItem.showDialog">
+                <DeleteSettingsDialog
+                    :id="deleteItem.itemId"
+                    :name="deleteItem.itemName"
+                    :type="deleteItem.itemType"
+                    @close-dialog="handleCloseDeleteDialog"
+                    @delete-dialog="handleDeleteDialog"
+                />
+            </template>
         </div>
     </div>
 </template>
@@ -86,13 +145,14 @@
 <script>
 import { ref } from 'vue'
 import axios from "axios";
+import { mapGetters } from 'vuex'
 import EditSettingDialog from "../components/EditSettingDialog.vue";
-import { mapGetters } from 'vuex';
-const apiURL = import.meta.env.VITE_API_URL
+import DeleteSettingsDialog from "../components/DeleteSettingsDialog.vue"
 
 export default {
     components: {
         EditSettingDialog,
+        DeleteSettingsDialog
     },
     data() {
         return {
@@ -103,18 +163,27 @@ export default {
                 itemName: '', // keeps track of the desc of the item (category or priority)
                 isItemActive: true, // // keeps track of the active status of the item (category or priority)
             },
-            categoryData: [],
-            priorityData: [],
+            deleteItem: {
+                itemType: '', // keeps track of it we're editing a category or priority
+                showDialog: false, // keeps track of if the edit dialog should pop open 
+                itemId: '', // keeps track of the id of the item (category or priority)
+                itemName: '', // keeps track of the desc of the item (category or priority)
+               
+            },
+            visibleCategoryColumns :['category_type', 'status', 'actions', 'delactions'], // Set visible columns of category table
+            visiblePriorityColumns: ['priorities', 'status', 'actions', 'delactions'], // Set visible columns of priorities table
+            categoryData: [],  // Holds the category data retrieved from the database
+            priorityData: [], // Holds the priority data retrieved from the database
+            updatedCategoryData:[],
+            tableKey: 0, // Used to re-render q-tables after a row has been deleted
+          
+          
         }
     },
     created() {
-        axios.get(`${apiURL}/categories`).then((res) => {
-            this.categoryData = res.data
-            this.loading = false
-        })
-        axios.get(`${apiURL}/priorities`).then((res) => {
-            this.priorityData = res.data
-        })
+        this.getPriorities();  // Method which calls the GET API for Priorities
+        this.getCategories();  // Method which calls the GET API for Categories      
+        this.hideColums() // handles the visibility of the delete columns based on user role
     },
     methods: {
         /**
@@ -144,10 +213,17 @@ export default {
             // show dialog
             this.editedItem.showDialog = true;
         },
+
         handleCloseDialog() {
             // close dialog
             this.editedItem.showDialog = false;
         },
+
+        handleCloseDeleteDialog() {
+            // close delete dialog
+            this.deleteItem.showDialog = false;
+        },
+
         handleSaveDialog(response) {
             // destruct to access id property name and item from the response after you clicked save on the dialog
             const { idPropertyName, item } = response;
@@ -173,18 +249,87 @@ export default {
             // close dialog
             this.editedItem.showDialog = false;
         },
+
+        handleDeleteDialog(response) {
+            // Destruct to access id property name and item from the response after you clicked delete on the dialog
+            const { idPropertyName, item } = response;
+            // Get id to use in the delete api for the item
+            const itemId = item[idPropertyName];
+            const itemTypetoDelete = (this.deleteItem.itemType).toLocaleLowerCase();
+            // Call delete api
+            axios.delete(`http://localhost:8001/api/${itemTypetoDelete}/${itemId}`);
+            this.tableKey += 1 ;this.getCategories();
+            console.log("Key updt: ", this.tableKey)
+            this.getCategories();
+            this.getPriorities();
+            // close delete dialog
+            this.deleteItem.showDialog = false; 
+        },
+
+        // Hide delete column is user role is not 'IT Teacher'
+        hideColums(){
+            if (this.userRole != 'IT Teacher'){
+                this.visibleCategoryColumns =  ['category_type', 'status', 'actions']
+                this.visiblePriorityColumns = ['priorities', 'status', 'actions']
+            }
+        }, 
+
+        // Call GET API for categories
+        getCategories(){
+            axios.get(`http://localhost:8001/api/categories`).then((res) => {
+            this.categoryData = res.data
+            console.log(this.categoryData)
+            this.loading = false
+        })
+        },
+
+        // Call GET API for priorities
+        getPriorities(){
+             // Retrieve priority data by calling GET priority API
+        axios.get(`http://localhost:8001/api/priorities`).then((res) => {
+            this.priorityData = res.data
+            this.loading = false
+        })
+        },
+
+        // Function for delete dialog
+        deleteRow(type, props) {
+            let itemId;
+            let itemDesc;
+            // get item values from props based on what type it is
+            if (type === 'Category') {
+                // get category id/desc
+                itemId = props.row.TICKET_CATEGORY_ID;
+                itemDesc = props.row.TICKET_CATEGORY_DESC;
+            } else {
+                // get priority id/desc
+                itemId = props.row.TICKET_PRIORITY_ID;
+                itemDesc = props.row.TICKET_PRIORITY_DESC;
+            }
+           // update component data variables
+            this.deleteItem.itemId = itemId;
+            this.deleteItem.itemName = itemDesc;
+            this.deleteItem.itemType = type;
+            // show dialog
+            this.deleteItem.showDialog = true;
+        },
     },
+
     setup() {
         const catColumns = [
             { name: 'category_type', label: 'Category', field: 'TICKET_CATEGORY_DESC', align: 'left' },
             { name: "status", align: "center", label: "Status", field: "ACTIVE_STATUS_DESC", sortable: true },
             { name: 'actions', label: 'Edit', field: '', align: 'left' },
+            { name: 'delactions', label: 'Delete', field: '', align: 'left' },
         ];
         const prioritiesColumns = [
             { name: 'priorities', label: 'Priorities', field: 'TICKET_PRIORITY_DESC', align: 'left' },
             { name: "status", align: "center", label: "Status", field: "ACTIVE_STATUS_DESC", sortable: true },
             { name: 'actions', label: 'Edit', field: '', align: 'left' },
+            { name: 'delactions', label: 'Delete', field: '', align: 'left' },
         ];
+
+
         const statuses = ['Active', 'Inactive'];
         return {
             tab: ref('categories'),
@@ -194,8 +339,20 @@ export default {
             prioritiesColumns,
             selected: ref([]),
             redModel: ref(true),
+         
         };
     },
+    computed: {
+      ...mapGetters('auth', ['isAuthenticated', 'userEmail', 'userFirstName', 'userRole']),
+    }
 }
 </script>
-  
+
+<style lang="sass">
+
+.q-table__top,
+
+thead tr:first-child th
+  /* bg color is important for th; just specify one */
+  background-color: #e6e2e2
+</style>
