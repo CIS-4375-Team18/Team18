cf8b09fc040f83b24c6bcfb2d9331cd26829e5f7
TjaartNaude <77753514+TjaartNaude@users.noreply.github.com>
2023-10-14 16:14:25 -0500

    Update Delete Dialog
    Updated delete dialog to accommodate "Main Categories," Hardware Categories," and "Software Categories."
    Associated with change log id: CL-1

--- CIS_4375_Project_App/Frontend/src/components/EditSettingDialog.vue
+++ CIS_4375_Project_App/Frontend/src/components/EditSettingDialog.vue
@@ -1,114 +1,129 @@
 <template>
-    <q-dialog v-model="showDialog" persistent>
-        <q-card class="edit-card" style="min-width: 350px">
-            <q-card-section class="qc-header q-pa-md text-white">
-                <div class="header-text">{{title}}</div>
-            </q-card-section>
+  <q-dialog v-model="showDialog" persistent>
+    <q-card class="edit-card" style="min-width: 350px">
+      <q-card-section class="qc-header q-pa-md text-white">
+        <div class="header-text">{{ title }}</div>
+      </q-card-section>
 
-            <template v-if="isAuthenticated && (userRole==='System Administrator' || userRole==='IT Teacher')">
-                <q-card-section >
-                    <q-input
-                    label="Category Name"
-                    dense
-                    v-model="itemName"
-                    autofocus />
-                </q-card-section>
-            </template>
+      <template
+        v-if="
+          isAuthenticated &&
+          (userRole === 'System Administrator' || userRole === 'IT Teacher')
+        "
+      >
+        <q-card-section>
+          <q-input label="Category Name" dense v-model="itemName" autofocus />
+        </q-card-section>
+      </template>
 
-            <q-card-section class="q-pt-none">
-                <q-toggle
-                    label="Status"
-                    v-model="isItemActive"
-                    checked-icon="check"
-                    color="green"
-                    unchecked-icon="clear"
-                    left-label
-                />
-                <span v-if="isItemActive">ACTIVE</span>
-                <span v-if="!isItemActive">INACTIVE</span>
-            </q-card-section>
+      <q-card-section class="q-pt-none">
+        <q-toggle
+          label="Status"
+          v-model="isItemActive"
+          checked-icon="check"
+          color="green"
+          unchecked-icon="clear"
+          left-label
+        />
+        <span v-if="isItemActive">ACTIVE</span>
+        <span v-if="!isItemActive">INACTIVE</span>
+      </q-card-section>
 
-            <q-card-actions align="right" class="text-primary">
-                <q-btn
-                flat
-                color="primary"
-                class="actionbtns"
-                label="Cancel"
-                @click="closeDialog"
-                />
-                <q-btn
-                color="primary"
-                class="actionbtns"
-                label="Save"
-                @click="saveChanges" />
-            </q-card-actions>
-        </q-card>
-    </q-dialog>
+      <q-card-actions align="right" class="text-primary">
+        <q-btn
+          flat
+          color="primary"
+          class="actionbtns"
+          label="Cancel"
+          @click="closeDialog"
+        />
+        <q-btn
+          color="primary"
+          class="actionbtns"
+          label="Save"
+          @click="saveChanges"
+        />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters } from "vuex";
 export default {
-    data() {
-        return {
-            title: '',
-            nameLabel: '',
-            showDialog: true,
-            itemId: 0,
-            itemName: '',
-            isItemActive: true,
-        };
-    },
-    props: {
-        // item id
-        id: Number,
-        // item name
-        name: String,
-        // item active status
-        isActive: Boolean,
-        // item type
-        type: String,
-    },
-    created() {
-        // dynamically create title name based on what type it is
-        // e.g. "Update Category" or "Update Priority"
-        this.title = `Edit ${this.type}`;
-        // set variables used for save api later
-        this.itemId = this.id;
-        this.itemName = this.name;
-        this.isItemActive = this.isActive;
-    },
-    methods: {
-        closeDialog() {
-            // hide dialog
-            this.showDialog = false;
-            // notify parent component to close dialog
-            this.$emit("close-dialog", false);
-        },
-        saveChanges() {
-            // create dynamic property name
-            // e.g. TICKET_CATEGORY_ID or TICKET_PRIORITY_ID
-            const idPropertyName = `TICKET_${this.type.toUpperCase()}_ID`;
-            // e.g. TICKET_CATEGORY_ID or TICKET_PRIORITY_DESC
-            const descPriortyName = `TICKET_${this.type.toUpperCase()}_DESC`;
-
-            // notify parent to save dialog with the following values
-            this.$emit("save-dialog", {
-                item: {
-                    [idPropertyName]: this.itemId,
-                    [descPriortyName]: this.itemName,
-                    ACTIVE_STATUS_ID: this.isItemActive ? 1 : 2, // 1 is active, 2 is inactive
-                    ACTIVE_STATUS_DESC: this.isItemActive ? "ACTIVE" : "INACTIVE",
-                },
-                idPropertyName,
-            });
-        },
-
-
-    },
-    computed: {
-      ...mapGetters('auth', ['isAuthenticated', 'userEmail', 'userFirstName', 'userRole']),
+  data() {
+    return {
+      title: "",
+      nameLabel: "",
+      showDialog: true,
+      itemId: 0,
+      itemName: "",
+      isItemActive: true,
+      idPropertyName: "",
+      descPriortyName: "",
+      headerDesc: "",
+    };
+  },
+  props: {
+    // item id
+    id: Number,
+    // item name
+    name: String,
+    // item active status
+    isActive: Boolean,
+    // item type
+    type: String,
+  },
+  created() {
+    // dynamically create title name based on what type it is
+    if (this.type == 'HARDW_SUB_CATEGORY'){
+      this.headerDesc = 'Edit Hardware Sub Category';
+    } else if (this.type == 'SOFTW_SUB_CATEGORY'){
+      this.headerDesc = 'Edit Software Category';
+    } else{
+      this.headerDesc = 'Edit Main Category';
     }
+    this.title = `${this.headerDesc}`;
+    // set variables used for save api later
+    this.itemId = this.id;
+    this.itemName = this.name;
+    this.isItemActive = this.isActive;
+  },
+  methods: {
+    closeDialog() {
+      // hide dialog
+      this.showDialog = false;
+      // notify parent component to close dialog
+      this.$emit("close-dialog", false);
+    },
+    saveChanges() {
+      let idPropertyName = "";
+      let descPriortyName = "";
+      // create dynamic property name
+      // e.g. TICKET_CATEGORY_ID or HARDW_SUB_CATEGORY_ID
+      idPropertyName = `${this.type.toUpperCase()}_ID`;
+      // e.g. TICKET_CATEGORY_ID or HARDW_SUB_CATEGORY_DESC
+      descPriortyName = `${this.type.toUpperCase()}_DESC`;
+      // notify parent to save dialog with the following values
+      this.$emit("save-dialog", {
+        item: {
+          [idPropertyName]: this.itemId,
+          [descPriortyName]: this.itemName,
+          ACTIVE_STATUS_ID: this.isItemActive ? 1 : 2, // 1 is active, 2 is inactive
+          ACTIVE_STATUS_DESC: this.isItemActive ? "ACTIVE" : "INACTIVE",
+        },
+        idPropertyName,
+      });
+    },
+  },
+  computed: {
+    ...mapGetters("auth", [
+      "isAuthenticated",
+      "userEmail",
+      "userFirstName",
+      "userRole",
+    ]),
+  },
 };
 </script>
 
@@ -117,8 +132,8 @@ export default {
   margin: 10px;
 }
 .qc-header {
-border-bottom: 0.2rem solid #a62626;
-background-color: #666262;
+  border-bottom: 0.2rem solid #a62626;
+  background-color: #666262;
 }
 .header-text {
   font-size: 1.1rem;
