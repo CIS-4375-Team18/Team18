c66539dd050cd2a201c686d70d76a35143fb2714
anguyen1112 <alain141211@gmail.com>
2023-10-17 22:44:10 -0700

    display all requests with subject and creation date

--- CIS_4375_Project_App/Frontend/src/pages/RequestsPage.vue
+++ CIS_4375_Project_App/Frontend/src/pages/RequestsPage.vue
@@ -1,10 +1,59 @@
 <template>
-
-<h3 class="text-center">Requests Page</h3>
-
-  </template>
+    <div class="q-pa-md">
+        <div class="q-gutter-y-md" style="margin-left: 15px; margin-right: 15px;">
+            <q-card>
+                <q-tabs v-model="tab" dense class="text-white" indicator-color="white" :align="'justify'" narrow-indicator
+                    style="background-color: #ad0000;">
+                    <q-tab name="requests" label="View Requests" />
+                </q-tabs>
+                <q-separator />
+                <q-tab-panels v-model="tab" animated :align="'center'">
+                    <q-tab-panel name="requests">
+                        <div class="q-pa-md">
+                            <q-table title="Requests" color="secondary" :align="left" :loading="loading"
+                                :rows="supporttickets" :columns="supportticketsColumns" style="width: 80%;">
+                            </q-table>
+                        </div>
+                    </q-tab-panel>
+                </q-tab-panels>
+            </q-card>
+        </div>
+    </div>
+</template>
   
-  <script>
-  
-  </script>
+<script>
+import { ref } from 'vue'
+import axios from "axios";
+const apiURL = import.meta.env.VITE_API_URL
+
+export default {
+    data() {
+        return {
+            supporttickets: [],
+        }
+    },
+    created() {
+        axios.get(`${apiURL}/supporttickets`).then((res) => {
+            this.supporttickets = res.data
+            this.loading = false
+        });
+    },
+    methods: {
+    },
+    setup() {
+        const supportticketsColumns = [
+            { name: 'subject', label: 'Subject', field: 'SUPPORT_TICKET_SUBJECT', align: 'left' },
+            { name: "creationDate", align: "left", label: "Creation Date", field: "SUPPORT_TICKET_DATE_CREATED", sortable: true, format: (val) => `${new Date(val)}` },
+        ];
+        return {
+            tab: ref('requests'),
+            setActive: ref('Active'),
+            loading: ref(true),
+            supportticketsColumns,
+            selected: ref([]),
+            redModel: ref(true),
+        };
+    },
+}
+</script>
   
